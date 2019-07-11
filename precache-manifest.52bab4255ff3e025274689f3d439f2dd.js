7b7b12ef7b239671c748e747d5378a29d9b57bbb
buley004 <colinbuley@D-10-19-216-189.dhcp4.washington.edu>
2019-07-10 19:59:05 -0700

    Updates

---
+++ precache-manifest.52bab4255ff3e025274689f3d439f2dd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f90ae738ef39512e9e58eb3871bb41ef",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "5b1798cf238bc071a3b7",
+    "url": "/clicky-game/static/css/main.03a5808e.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/clicky-game/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "5b1798cf238bc071a3b7",
+    "url": "/clicky-game/static/js/main.d8dac570.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  }
+]);
